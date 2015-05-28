82fe8543309b6985a23e8ebe1579e426d3915ccc
Sydney Lindsay <sydneyjlindsay@gmail.com>
2015-05-27 22:33:39 -0500

    final commit

--- dist/scripts/controllers.js
+++ dist/scripts/controllers.js
@@ -2,18 +2,17 @@ $(document).ready(function() {
 	
 	var $btn = $('button')
 
-	$btn.data('count', 0); //set the data key to 'count' and the number to 0
+	$btn.data('count', 0); 
 
-	$btn.click(function(){ //add event listener, on click...
+	$btn.click(function(){ 
 
-	    $btn.html(function(){ //on click, change the html of the button
-	        var numberLikes = $btn.data('count') + 1; //set number of likes variable inside this function to btn data count +1
-			$btn.data('count', numberLikes); //set button data equal to count (which is 0) + number of likes
-	        if(numberLikes === 1) {
-	        	return numberLikes+(" Like"); //if number of likes is 1, put Like in the button
+	    $btn.html(function(){ 
+	        var numberLikes = $btn.data('count') + 1; 
+			$btn.data('count', numberLikes); 
+	        	return numberLikes+(" Like"); 
 	        }
 	        else {
-	        	return numberLikes+(" Likes"); //if likes is more than 1, or 0, put Likes
+	        	return numberLikes+(" Likes");
 	        }
 	    });
 	});
