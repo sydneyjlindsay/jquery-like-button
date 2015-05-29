2dbf5c7c0b9003d8c7491c047ba1d5e72331f6b1
Sydney Lindsay <sydneyjlindsay@gmail.com>
2015-05-28 21:18:48 -0500

    corrected/resubmit

--- dist/scripts/controllers.js
+++ dist/scripts/controllers.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	
-	var $btn = $('button')
+	var $btn = $('button');
 
 	$btn.data('count', 0); 
 
@@ -9,11 +9,12 @@ $(document).ready(function() {
 	    $btn.html(function(){ 
 	        var numberLikes = $btn.data('count') + 1; 
 			$btn.data('count', numberLikes); 
+			  if(numberLikes === 1) {
 	        	return numberLikes+(" Like"); 
 	        }
 	        else {
 	        	return numberLikes+(" Likes");
-	        }
+	        };
 	    });
 	});
 });
